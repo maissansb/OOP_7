3c28de4a1ee0ab4b6cbe8aacae2695cf28b407af
Maissan Salim <maissansb@gmail.com>
2020-10-14 01:42:14 +0400

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,24 @@
+//
+// Created by LENOVO 3 on 10/13/2020.
+//
+#include <iostream>
+#include "person.h"
+#include "patient.h"
+#include "doctor.h"
+#include <string>
+using namespace std;
+
+void person::set(string n,int m){
+    name=n;
+    money=m;
+}
+int getMoney(){
+
+}
+person::person(string n, int m){
+    name=n;
+    money=m;
+}
+void person::print(){
+    cout<<name<<" has "<<money<<" dirhams and treated the following patients:";
+}
---
+++ person.h
@@ -0,0 +1,26 @@
+//
+// Created by LENOVO 3 on 10/13/2020.
+//
+
+#ifndef LAB6_PERSON_H
+#define LAB6_PERSON_H
+
+#include "person.h"
+#include "patient.h"
+#include "doctor.h"
+#include <string>
+using namespace std;
+class person {
+private:
+    string name;
+    int money;
+public:
+    void set(string, int);
+    int getMoney();
+    void print();
+    person(string="",int=0);
+
+};
+
+
+#endif //LAB6_PERSON_H
