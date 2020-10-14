cf17144c98d8fe088838b24415f8a4f95bdf10c0
Maissan Salim <maissansb@gmail.com>
2020-10-14 14:04:19 +0400

    Update patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,4 +1,27 @@
 //
 // Created by LENOVO 3 on 10/13/2020.
 //
+#include <iostream>
+#include "person.h"
+#include "patient.h"
+#include "doctor.h"
+#include <string>
+using namespace std;
 
+void patient::set(){
+    cin>>name>>durationOfTreatment>>TreatmentCost;
+}
+int patient:: getCost(){
+    return TreatmentCost;
+}
+int patient::getDuration() {
+    return durationOfTreatment;
+}
+patient::patient(string n,int d,int c){
+    name=n;
+    durationOfTreatment=d;
+    TreatmentCost=c;
+}
+void print(){
+cout<<name<<" was treated for "<<durationOfTreatment<<" minutes, at a cost "<<TreatmentCost<< " dirham per minute "<<endl;
+}
